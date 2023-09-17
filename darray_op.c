607c6eeb6421df32e2f316e4b53f7650a7aa0d8b
Sudeep S D <119334116+SUDEEPSD28@users.noreply.github.com>
2023-09-17 12:43:57 +0530

    darray_op.c

--- darray_op.c
+++ darray_op.c
@@ -33,5 +33,28 @@ int search_data(Array_dyn *dynarr,int element)
     }
     return NOT_FOUND;
 }
+Array_dyn * deallocate(Array_dyn *dynarr)
+{
+    free(dynarr->arr);
+    free(dynarr);
 
+    return NULL;
+
+}
+Array_dyn * intersection(Array_dyn *arr_a, Array_dyn *arr_b)
+{
+    Array_dyn *arr_c;
+    int i;
+
+    if(arr_a->csize == 0 || arr_b->csize == 0) return NULL;
+
+    arr_c = initialize_array(arr_a->csize);
+
+    for(i=0; i<arr_a->csize; i++)
+    {
+        if(search_data(arr_b, arr_a->arr[i]) && !search_data(arr_c, arr_a->arr[i] ))
+                assert(insert_data(arr_c, arr_a->arr[i]));
+    }
+    return arr_c;
+}
 
